2bf5b1fdb9d636c5a9b6dfbceca85885824c8e0b
Raphaël Heng <raphael.heng@epita.fr>
2022-04-26 23:41:02 +0200

    save

--- python_files/calculation.py
+++ python_files/calculation.py
@@ -3,10 +3,12 @@ def get_average (low_sens, high_sens):
 
 def get_new_sens (pref, sens_f, sens_h, avg):
     if pref == "f":
+        print("")
         print("Nouvelle sensi faible : {}".format(sens_f))
         print("Nouvelle sensi haute : {}".format(avg))
         return (sens_f, avg)
     elif pref == "h":
+        print("")
         print("Nouvelle sensi faible : {}".format(avg))
         print("Nouvelle sensi haute : {}".format(sens_h))
         return (avg, sens_h)
