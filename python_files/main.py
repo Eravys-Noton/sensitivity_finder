4c61f0d00b51e033674a597d6b4fa62b8b509251
Raphaël Heng <raphael.heng@epita.fr>
2022-04-26 23:30:29 +0200

    save

--- python_files/main.py
+++ python_files/main.py
@@ -5,6 +5,8 @@ def print_introduction ():
     print("Pour faire ce test vous aurez besoin de trouver une cible immobile.")
     print("Pour tester vos préférences de sensibilités, essayez de garder votre viseur sur la cible tout en vous déplaçant.")
     print("La sensibilité avec laquelle vous visez le mieux la cible est votre sensibilité préférée.")
+    print("La sensibilité faible de départ doit être une sensi qui vous semble très lente, genre Papy et tout.")
+    print("La sensiblité haute de départ doit être une sensi qui vous semble très rapide, genre Parkinson et tout.")
     print("")
 
 def sens_finder ():
