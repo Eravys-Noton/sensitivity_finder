8bd2433d2f0ede7cc2aedee6bb517c31c11abe65
Raphaël Heng <raphael.heng@epita.fr>
2022-04-27 18:34:58 +0200

    save

--- python_files/sensitivity_finder.py
+++ python_files/sensitivity_finder.py
@@ -1,13 +1,29 @@
 import calculation
 import user_input
+import time
 
 def print_introduction ():
-    print("Pour faire ce test vous aurez besoin de trouver une cible immobile et SURTOUT VOUS NE DEVEZ PAS ETRE ECHAUFFE.")
-    print("Pour tester vos préférences de sensibilités, essayez de garder votre viseur sur la cible tout en vous déplaçant.")
-    print("La sensibilité avec laquelle vous visez le mieux la cible est votre sensibilité préférée.")
-    print("La sensibilité faible de départ doit être une sensi qui vous semble très lente, genre Papy et tout.")
-    print("La sensiblité haute de départ doit être une sensi qui vous semble très rapide, genre Parkinson et tout.")
-    print("")
+    sentences = [
+        "IMPORTANT A LIRE :",
+        "Pour faire ce test vous aurez besoin de trouver une cible immobile et SURTOUT VOUS NE DEVEZ PAS ETRE ECHAUFFE.",
+        "Pour tester vos préférences de sensibilités, essayez de garder votre viseur sur la cible tout en vous déplaçant.",
+        "La sensibilité avec laquelle vous visez le mieux la cible est votre sensibilité préférée.",
+        "Pour les nombres à virgule, ils doivent être saisis avec des POINTS et non des VIRGULES.",
+        "La sensibilité faible de départ doit être une sensi qui vous semble très lente, genre Papy et tout.",
+        "La sensiblité haute de départ doit être une sensi qui vous semble très rapide, genre Parkinson et tout.",
+        "Lors du test, il est normal de préférer un coup la sensi faible et un coup la sensi haute.",
+        ""
+    ]
+    for sentence in sentences:
+        print(sentence)
+
+    for s in range(10, 0, -1):
+        loading = "Le test commence dans : " + str(s)
+        print(loading, end="\r")
+        time.sleep(1)
+
+    print("DEBUT DU TEST : ")
+
 
 def sens_finder ():
     print_introduction()
